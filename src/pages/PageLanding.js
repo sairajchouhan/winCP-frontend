503c8399f70ef5b5c6c81dd5ecf16af7ead69aa1
sairaj <sairaj2119@gmail.com>
2020-10-24 20:01:52 +0530

    initial setup

---
+++ src/pages/PageLanding.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PageLanding = () => {
+  return <div>lanidng page</div>;
+};
+
+export default PageLanding;
---
+++ src/pages/PageSignup.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PageSignup = () => {
+  return <div>signup</div>;
+};
+
+export default PageSignup;
