d658cc8ad5ab895427d0f15c12d57e48f4d143d7
sairaj <sairaj2119@gmail.com>
2020-11-07 19:14:13 +0530

    refactored navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -10,6 +10,12 @@ import { selectIsAuthenticated } from '../../redux/slices/authSlice';
 import AddIcon from '@material-ui/icons/Add';
 import { logoutUser } from '../../redux/actions/authActions';
 import Container from '@material-ui/core/Container';
+import Badge from '@material-ui/core/Badge';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import Avatar from '@material-ui/core/Avatar';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Tooltip from '@material-ui/core/Tooltip';
 
 import { getAllWins } from '../../redux/actions/winsActions';
 
@@ -26,6 +32,13 @@ const useStyles = makeStyles((theme) => ({
     color: 'white',
     textDecoration: 'none',
   },
+  button: {
+    color: 'white',
+  },
+  avatar: {
+    width: theme.spacing(4),
+    height: theme.spacing(4),
+  },
 }));
 
 const Navbar = () => {
@@ -33,10 +46,20 @@ const Navbar = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
   const history = useHistory();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const handleLogout = async () => {
     localStorage.removeItem('token');
     await dispatch(logoutUser());
+    handleClose();
     history.push('/');
   };
 
@@ -63,15 +86,47 @@ const Navbar = () => {
               <></>
             ) : isAuthenticated === true ? (
               <>
-                <Button color="inherit" component={Link} to="/create-post">
-                  <AddIcon />
+                <Tooltip title="Add" placement="bottom">
+                  <Button color="inherit" component={Link} to="/create-post">
+                    <AddIcon />
+                  </Button>
+                </Tooltip>
+                <Tooltip title="Notifications" placement="bottom">
+                  <Button className={classes.button}>
+                    <Badge badgeContent={4} color="secondary">
+                      <NotificationsIcon color="white" />
+                    </Badge>
+                  </Button>
+                </Tooltip>
+
+                <Button
+                  aria-controls="simple-menu"
+                  aria-haspopup="true"
+                  onClick={handleClick}
+                >
+                  <Avatar
+                    className={classes.avatar}
+                    alt="Remy Sharp"
+                    src="/static/images/avatar/1.jpg"
+                  />
                 </Button>
-                <Button color="inherit" component={Link} to="/profile">
-                  My Profile
-                </Button>
-                <Button onClick={handleLogout} color="inherit">
-                  Logout
-                </Button>
+                <Menu
+                  id="simple-menu"
+                  anchorEl={anchorEl}
+                  keepMounted
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                  <MenuItem
+                    component={Link}
+                    to="/profile"
+                    onClick={handleClose}
+                  >
+                    Profile
+                  </MenuItem>
+                  <MenuItem onClick={handleClose}>My account</MenuItem>
+                  <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                </Menu>
               </>
             ) : (
               <>
